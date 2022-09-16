3d8387d4fd95a15391cf9a8ba89c00cccdb40aa6
Ilyha322 <tsygankoff.ilia@ya.ru>
2022-09-16 18:12:21 +0300

    create file les1

--- les1.py
+++ les1.py
@@ -1,1 +1,11 @@
-"asdfasfasfasfsa"
+
+per1 = "hello"
+per12 = "world"
+per2 = 34
+per22 = 35
+per3 = 554.343
+per32 = 521.213
+per4 = None
+per5 = True
+per6 = False
+print(per1,per12,per2,per22,per3,per32,per4,per5,per6)
